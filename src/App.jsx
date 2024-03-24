ee81faa1ea0bb3363593b44e0fcb096cde72c591
unknown <bazanjuanjordan@gmail.com>
2024-03-23 22:55:41 -0300

    se crea rutas en app jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import RoutesFront from "./routes/RoutesFront";
+
+const App = ()=>{
+  <>
+  <Router>
+    <RoutesFront/>
+  </Router>
+  </>
+}
+
+export default App

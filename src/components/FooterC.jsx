bd05ec8f8ec82e022dd0c92e20be0855321b4747
unknown <bazanjuanjordan@gmail.com>
2024-03-27 19:52:41 -0300

    Se crea footer basico

--- src/components/FooterC.jsx
+++ src/components/FooterC.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import '../css/Footer.css'
 const FooterC = () => {
   return (
-    <div>
-      Footer
-    </div>
+   <>
+    <footer className="footer">
+        <div className="container mt-2">
+            <div className="row text-center py-2">
+      
+                <div className="col-sm-12 col-md-12 col-lg-12">
+                <h5 className="">Contacto</h5>
+                <h5 className="">ControStock@gmail.com</h5>
+                <h5 className="">Tel: 3815235689</h5>
+                            
+                    
+                </div>
+            </div >
+        </div >
+    </footer >
+    </>
   )
 }
 

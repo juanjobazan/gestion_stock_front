77bb80e4446a985f64803734bfe824454ee43164
unknown <bazanjuanjordan@gmail.com>
2024-04-07 22:36:39 -0300

    se crea pagina de busqueda de clientes

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -1,10 +1,71 @@
 import React from 'react'
-
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const CustomerPage = () => {
   return (
-    <div>
-      Clientes
-    </div>
+    <>
+      <div className='container my-5'>
+      <div className='d-flex justify-content-center'>
+            <Form >
+              <Row>
+                <Col xs="auto">
+                  <Form.Control
+                    type="text"
+                    placeholder="Search"
+                    className=" mr-sm-2"
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button type="submit">Submit</Button>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        <div className='d-flex justify-content-center mt-5'>
+          
+          <Table striped bordered hover size='sm'>
+            <thead >
+              <tr>
+                <th>DNI</th>
+                <th>Nombre Y Apellido</th>
+                <th>Email</th>
+                <th>Direccion</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td colSpan={2}>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </>
   )
 }
 

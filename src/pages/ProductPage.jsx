5963dce79313a0b04912a44afe5e440536e5a26a
unknown <bazanjuanjordan@gmail.com>
2024-04-25 23:44:19 -0300

    Se crea conexion a base de datos y baja de productos

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table';
+import clienteAxios, { config } from '../../utils/axiosCliente';
+import { Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 const ProductPage = () => {
+    const [allProd, setAllProd] = useState([])
+    const [refreshState, resRefrestate] = useState(false)
+    const getAllProd = async () => {
+        const res = await clienteAxios.get('/product', config)
+        setAllProd(res.data)
+    }
+
+    const handleClick= async(id)=>{
+
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+              confirmButton: 'btn btn-success',
+              cancelButton: 'btn btn-danger'
+            },
+            buttonsStyling: false
+          })
+
+          swalWithBootstrapButtons.fire({
+            title: 'Estas Seguro?',
+            text: "No podras Revertir Esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'SI, Eliminar!',
+            cancelButtonText: 'No, Cancelar!',
+            reverseButtons: true
+          }).then((result) => {
+            if (result.isConfirmed) {
+             clienteAxios.delete(`/product/${id}`,config)
+              .then(res=>{
+                if(res.status === 200 ){
+                  swalWithBootstrapButtons.fire(
+                    'Eliminado!',
+                    res.data.msg,
+                    'success' 
+                  )
+                  }
+              })
+              resRefrestate(true)
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'NO se Elimino :)',
+                'error'
+              )
+            }
+          })
+    }
+
+    useEffect(() => {
+        getAllProd()
+        resRefrestate()
+    }, [refreshState])
     return (
         <>
             <div className=' container my-5'>
@@ -16,26 +74,19 @@ const ProductPage = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td colSpan={2}>Larry the Bird</td>
-                                <td>@twitter</td>
-                                <td>@mdo</td>
-                            </tr>
+                            {
+                                allProd.map((prod) =>
+                                    <tr key={prod._id}>
+                                        <td>{prod.codigo}</td>
+                                        <td>{prod.descripcion}</td>
+                                        <td>{prod.precio}</td>
+                                        <td>{prod.stock}</td>
+                                        <td>
+                                            <Button variant='danger' onClick={() => { handleClick(prod._id) }} >Elimimar</Button>
+                                        </td>
+                                    </tr>)
+                            }
+
                         </tbody>
                     </Table>
                 </div>

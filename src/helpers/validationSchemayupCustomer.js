3ea3da77d36cb6ce81d815ffaf9de732604b6d6c
unknown <bazanjuanjordan@gmail.com>
2024-04-27 17:36:29 -0300

    se conecta alta de cliente con back

--- src/helpers/validationSchemayupCustomer.js
+++ src/helpers/validationSchemayupCustomer.js
@@ -1,9 +1,9 @@
 import * as yup from 'yup'
 
 const customerSchema = yup.object({
-nombreApellido: yup.string().required('El Campo Nombre y Apellido es Obligatorio').max(45,'El Maximo es de 45 Caracteres').min(8,'El Minimo es de 8 caracteres'),
+nombre: yup.string().required('El Campo Nombre y Apellido es Obligatorio').max(45,'El Maximo es de 45 Caracteres').min(8,'El Minimo es de 8 caracteres'),
 dni: yup.number().required('El Campo DNI es Obligatorio').min(9999999,'Faltan Numeros por Ingresar').max(99999999,'Supero el Maximo de Caracteres'),
-email: yup.string().required('El Campo Email es Obligatorio').email('No es un Email Valido'),
+mail: yup.string().required('El Campo Email es Obligatorio').email('No es un Email Valido'),
 direccion: yup.string().required('El Campo Direccion es Obligatorio').min(8,'El minimo es de 8 caracteres').max(32,'El Maximo es de 32 Caracteres')
 });
 

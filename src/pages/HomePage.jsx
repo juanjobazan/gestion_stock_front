45d632c67bb74dedcf808d7d9e260f5581b57fb3
unknown <bazanjuanjordan@gmail.com>
2024-05-03 21:40:20 -0300

    se crea buscador en home y mustra las card de los productos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SearchHome from '../components/SearchHome'
 
 const HomePage = () => {
   return (
-    <div>
-      Home Page
-    </div>
+  <>
+  <SearchHome></SearchHome>
+  </>
   )
 }
 
